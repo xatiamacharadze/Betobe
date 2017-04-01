06c34294a644300373959202600f6a2f30956d47
Xatia <kmach12@freeuni.edu.ge>
2017-04-01 19:56:48 +0400

    daily offers

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+    $('.daily-offers-list').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true
+    });
+});
