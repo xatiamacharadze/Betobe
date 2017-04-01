5a0c8a0f66968a94b3bff8330a4f9c16fe54bf77
Xatia <kmach12@freeuni.edu.ge>
2017-04-01 23:39:04 +0400

    special offers

--- js/main.js
+++ js/main.js
@@ -17,4 +17,14 @@ $(document).ready(function(){
         slidesToScroll: 1,
         centerMode: true
     });
+
+
+
+    $('.special-offer-images').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 });
